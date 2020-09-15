04d1672501a48e21301247f0a5247769f4033757
Christian Y Ocampo <58756433+pabloescobyte@users.noreply.github.com>
2020-09-14 20:14:58 -0400

    Finalized keymap with four layers and lowered power consumption config for iOS use.

--- keyboards/thevankeyboards/minivan/keymaps/pabloescobyte/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/pabloescobyte/keymap.c
@@ -2,42 +2,111 @@
 
 extern keymap_config_t keymap_config;
 
+
+// abbreviated modifier + keys
+// ALT+Left Arrow/Right Arrow
+// OPT+Left Arrow/Right Arrow in mac OS
+#define ALA A(KC_LEFT)
+#define ARA A(KC_RIGHT)
+
+// Hotkeys to select text from the current cursor position to the beginning or end of the current line
+// SHIFT+ALT Left Arrow/Right Arrow
+// SHIFT+OPT Left Arrow/Right Arrow in mac OS
+#define SALA S(A(KC_LEFT))  // select from current cursor position to beginning of current line
+#define SARA S(A(KC_RIGHT)) // select from current cursor position to end of current line
+
+// Hotkeys to move forward and backward through open browser tabs
+// CTRL+TAB/CTRL+SHIFT+TAB
+// CMD+TAB/CMD+SHIFT+TAB is mac OS
+#define CT C(KC_TAB)      // switch to next tab
+#define CST S(C(KC_TAB))  // switch to previous tab
+
+// Switch between open app windows in mac os
+// Simulates pressing CMD+~
+#define CMT A(KC_TILD)
+
+// FN+WASD for Arrows
+#define FNUP KC_UP
+#define FNDN KC_DOWN
+#define FNLA KC_LEFT
+#define FNRA KC_RIGHT
+
+// Abbreviate ESC, ENTER, SEMICOLON keys to keep grid uniform
+#define ES KC_ESC
+#define ET KC_ENT
+#define ENTR KC_ENT
+#define SC KC_SCLN
+
+// Media keys
+#define VOLU KC__VOLUP
+#define VOLD KC__VOLDOWN
+#define MUTE KC__MUTE 
+
+// Clipboard keys
+// Map to CMD+X/C/V on macOS when the modifiers
+// are mapped to CTRL, OPTION, CMD so CMD acts as "ALT"
+#define CUT A(KC_X)
+#define COPY A(KC_C)
+#define PAST A(KC_V)
+
+// Shifted number row
+#define S1 S(KC_1) // !
+#define S2 S(KC_2) // @
+#define S3 S(KC_3) // #
+#define S4 S(KC_4) // #
+#define S5 S(KC_5) // $
+#define S6 S(KC_6) // ^
+#define S7 S(KC_7) // &
+#define S8 S(KC_8) // *
+#define S9 S(KC_9) // (
+#define S0 S(KC_0) // )
+
+// Brackets, symbols often used when coding
+#define LSB KC_LBRC // [ left square bracket
+#define RSB KC_RBRC // ] right square bracket
+#define LCB S(KC_LBRC) // { left curly brace
+#define RCB S(KC_RBRC) // } right curly brace
+ #define LPT S(KC_9) // ( left parentheses
+#define RPT S(KC_0) // ) right parentheses
+
+// Switch between open windows on mac OS
+// Sends key combo CMD+`
+#define AGR A(KC_GRV)
+
+
+
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_arrow_command( /* Qwerty */
-    KC_GESC,       KC_Q,    KC_W,    KC_E,  KC_R, KC_T,    KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-    LT(2, KC_TAB), KC_A,    KC_S,    KC_D,  KC_F, KC_G,    KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, LT(2, KC_ENT),
-    KC_LSFT,       KC_Z,    KC_X,    KC_C,  KC_V, KC_B,    KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_UP,   MT(MOD_RSFT, KC_SLSH),
-    KC_LCTL,       KC_LGUI, KC_LALT, MO(3), LT(1, KC_SPC), LT(1, KC_SPC), MO(4),   KC_LEFT, KC_DOWN, KC_RIGHT
+  [0] = LAYOUT_arrow( /* Qwerty */
+    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,      KC_O,    KC_P,    KC_BSPC,
+    LT(1, ES), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,      KC_L,    KC_QUOT, ENTR,
+    KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,   KC_DOT,  KC_UP,   KC_SLSH,
+    KC_LCTL,   KC_LGUI, KC_LALT, LT(2, KC_SPC),             MO(3),            LT(4, SC), KC_LEFT, KC_DOWN, KC_RIGHT
   ),
-  [1] = LAYOUT_arrow_command( /* LAYER 2 */
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    KC_TRNS, KC_MINS, KC_EQL,  KC_SCLN, KC_QUOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL,  KC_QUOT, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_PGUP, KC_BSLS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS, KC_HOME, KC_PGDN, KC_END
+  [1] = LAYOUT_arrow( /* LAYER 2 */
+    AGR,       ALA,     FNUP,    ARA,     KC_HOME, KC_PGUP, VOLU,    LCB,     RCB,       KC_UNDS, KC_PLUS, KC_DEL,
+    KC_TRNS,   FNLA,    FNDN,    FNRA,    KC_END,  KC_PGDN, VOLD,    LSB,     RSB,       KC_MINS, KC_EQL,  KC_SCLN,
+    KC_TRNS,   SALA,    SARA,    COPY,    PAST,    CUT,     MUTE,    LPT,     RPT,       KC_LT,   KC_GT,   KC_BSLS,
+    KC_TRNS,   KC_TRNS, KC_TRNS, CT,                        CMT,              KC_TRNS,   KC_TILD, KC_GRV,  KC_PIPE
   ),
-  [2] = LAYOUT_arrow_command( /* LAYER 1 */
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-    KC_TRNS, KC_UNDS, KC_PLUS, KC_COLN, KC_DQUO, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_PLUS, KC_DQUO, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_PGUP, KC_PIPE,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS, KC_HOME, KC_PGDN, KC_END
+  [2] = LAYOUT_arrow( /* LAYER 1 */
+    KC_TILD,   S1,      S2,      S3,      S4,      S5,      S6,      S7,      S8,        S9,      S0,      KC_DEL,
+    KC_TRNS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,      KC_9,    KC_0,    KC_MINS,
+    KC_F12,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,
+    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,                   ENTR,             KC_DOT,    KC_SLSH, KC_MINS, KC_PLUS
   ),
-  [3] = LAYOUT_arrow_command( /* LAYER 3 */
-    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [3] = LAYOUT_arrow( /* Numpad for nav + Mouse */
+    CT,        KC_HOME, FNUP,    KC_PGUP, KC_TRNS, KC_ACL0, KC_BTN2, KC_WH_L, KC_MS_U,   KC_WH_R, KC_UNDS, KC_PLUS,
+    CST,       FNLA,    FNRA,    FNRA,    KC_TRNS, KC_ACL1, KC_BTN1, KC_MS_L, KC_MS_D,   KC_MS_R, KC_MINS, KC_EQL,
+    KC_TRNS,   KC_END,  FNDN,    KC_PGDN, KC_TRNS, KC_ACL2, KC_BTN3, KC_TRNS, KC_TRNS,   KC_TRNS, KC_WH_U, KC_TRNS,
+    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,          KC_TRNS,   KC_TRNS, KC_WH_D, KC_TRNS
   ),
-  [4] = LAYOUT_arrow_command( /* Gaming Layer*/
-    KC_ESC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS, KC_TRNS,
-    KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LT(5, KC_SLSH),
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_SPACE, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  ),
-  [5] = LAYOUT_arrow_command( /* RESET Layer*/
-    RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [4] = LAYOUT_arrow( /* Numpad, RESET */
+    KC_NLCK,   KC_P7,   KC_P8,   KC_P9,   KC_MINS, KC_PSLS, KC_TRNS, KC_TRNS, KC_INS,    KC_PAUS, KC_PSCR, KC_TRNS,
+    KC_TRNS,   KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_PAST, KC_TRNS, KC_TRNS, KC_TRNS,   KC_SLCK, KC_TRNS, KC_TRNS,
+    KC_TRNS,   KC_P1,   KC_P2,   KC_P3,   ENTR,    KC_BSPC, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,   KC_TRNS, KC_P0,   KC_PDOT,                   KC_TRNS,          KC_TRNS,   KC_TRNS, KC_TRNS, RESET
   )
 };
 
@@ -46,10 +115,11 @@ void keyboard_post_init_user(void) {
     // Set up RGB effects on _only_ the third LED (index 2)
     rgblight_set_effect_range(2, 1);
     // Set LED effects to breathing mode in a tealish blue color
-    rgblight_sethsv_noeeprom(185, 255, 255);
-    rgblight_mode_noeeprom(RGBLIGHT_EFFECT_BREATHING + 2);
+    rgblight_sethsv_noeeprom(185, 255, 1);
+    rgblight_disable();
+    //rgblight_mode_noeeprom(RGBLIGHT_EFFECT_BREATHING + 2);
 
-    // Init the first two LEDs to a static color
+    // Init the first two LEDs to a static color              
     setrgb(0, 0, 0, (LED_TYPE *)&led[0]);
     setrgb(0, 0, 0, (LED_TYPE *)&led[1]);
     rgblight_set();
@@ -62,17 +132,17 @@ uint32_t layer_state_set_user(uint32_t state){
     uint8_t led1r = 0; uint8_t led1g = 0; uint8_t led1b = 0;
 
     if (layer_state_cmp(state, 1)) {
-      led0r = 255;
+      led0r = 50;
     }
     if (layer_state_cmp(state, 2)) {
-      led0g = 255;
+      led0g = 50; 
     }
-
+    if (layer_state_cmp(state, 3)) {
+      led1b = 50;
+    }
     if (layer_state_cmp(state, 4)) {
-      led1b = 255;
-    }
-    if (layer_state_cmp(state, 5)) {
-      led1r = 255;
+      led1b = 50;
+      led1r = 50;
     }
 
     setrgb(led0r, led0g, led0b, (LED_TYPE *)&led[0]);
--- keyboards/thevankeyboards/minivan/keymaps/pabloescobyte/readme.md
+++ keyboards/thevankeyboards/minivan/keymaps/pabloescobyte/readme.md
@@ -1,33 +1,19 @@
-# Pabloescobyte's Catalyst minivan layout
+# PabloEscobyte's minivan layout
 
-This layout is for a Catalyst MiniVan with the arrow layout and the standard 3-modifier, split-space configuration.
-
-There are 4 layers in addition to the default base layer.
-
-The CAPS key is FN1, left spacebar is FN2, right spacebar is FN3; the modifier between the right spacebar and the left arrow key is FN4 but is a toggle rather than a momentary layer switch.
-
-Additionally CAPS acts as the ESC key when tapped otherwise it's a momentary layer toggle when held down. I do this because of muscle memory and I don't need to hit ESC all that often. 
+My personal minivan layout is built specifically for my day-to-day work in mac OS catalina with a catalyst (hotswap) PCB and tthe arrow layout.
 
 
-My work is primarily based in mac OS and Linux so the functions are tailored as such. If you work in Windows you can easily adjust for it by changing the aliases at the top of the file.  
 
-By default the minivan modifiers on the bottom left are CTRL+GUI+ALT and I just swap them around via the 'Modifier Keys' dialog in System Preferences 'Keyboard' preference pane to CTRL/OPTION/COMMAND. This is to preserve their normal positions in Linux and Windows or any other machine the keyboard is plugged into besides my own.
+## Base layer
 
-
-## Default/Base Layer (0)
-
-The base layer is the default QWERTY layout with TAB to the left instead of ESC. The CAPS key acts as ESC when tapped otherwise it's a momentary layer toggle to FN layer 1.
-
-/? is off to the right of the UP arrow key and does take time to get used to. I do use the forward slash a lot when in the command line so I've already gotten used to its new location.
-
-
+The base layer is pretty simple, straight qwerty layout where available. Of note: /? is to the right of the arrow keys. Yes this is kinda weird sometimes, but you get used to it. Tab is tab when pressed and fn2 when held, Enter is the same. GESC is esc when used alone, but ~ when shifted. Tapping FN 1 is space
 ```
 |GESC| Q  | W  | E  | R  | T  | Y  | U  | I  | O  | P  | BSPC |
 | TAB | A  | S  | D  | F  | G  | H  | J  | K  | L  | ;  |ENTER|
 |SHIFT | Z  | X  | C  | V  | B  | N  | M  | ,  | .  | UP | /  |
 |CTRL|WIN |ALT |FN 3|   FN 1   |  SPACE | FN 4 |LEFT|DOWN|RIGH|
 ```
-## Numeric Layer (1)
+## Navigation layer (1)
 
 This layer has the numrow as well as swapping the arrow keys for a nav cluster, and swapping ;: for '" and /? for \|. With that almost all of the missing keys are accounted for.
 ```
